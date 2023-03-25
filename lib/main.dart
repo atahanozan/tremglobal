acb7dd874b0cc9c1b9d1043a7e83efa20f11ef5b
atahanozan <atahanozantokdemir@gmail.com>
2023-03-26 01:26:34 +0300

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:tremglobal/firebase_options.dart';
 import 'package:tremglobal/projectpages/homepage.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
